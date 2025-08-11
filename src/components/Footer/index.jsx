9d31cb3690223b48a5fb2fa1736d93a8e0b86d0b
Mustapha Shuaibu <almussanplanner12@gmail.com>
2025-08-11 07:32:09 +0100

    updated mail

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,34 +1,47 @@
-import styles from "./style.module.css"
-import Image from "next/image"
-import Rounded from "../../common/RoundedButton"
-import { useRef } from "react"
-import { useScroll, motion, useTransform, useSpring } from "framer-motion"
-import Magnetic from "../../common/Magnetic"
+import styles from "./style.module.css";
+import Image from "next/image";
+import Rounded from "../../common/RoundedButton";
+import { useRef } from "react";
+import { useScroll, motion, useTransform, useSpring } from "framer-motion";
+import Magnetic from "../../common/Magnetic";
 
 export default function index() {
-  const container = useRef(null)
+  const container = useRef(null);
 
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start end", "end end"]
-  })
+    offset: ["start end", "end end"],
+  });
 
-  const x = useTransform(scrollYProgress, [0, 1], [0, 100])
-  const y = useTransform(scrollYProgress, [0, 1], [-500, 0])
+  const x = useTransform(scrollYProgress, [0, 1], [0, 100]);
+  const y = useTransform(scrollYProgress, [0, 1], [-500, 0]);
 
   return (
-    <motion.div style={{ y }} ref={container} className={styles.contact} id="contact">
+    <motion.div
+      style={{ y }}
+      ref={container}
+      className={styles.contact}
+      id="contact"
+    >
       <div className={styles.body}>
         <div className={styles.title}>
           <span>
             <div className={styles.imageContainer}>
               <Image fill={true} alt={"image"} src={`/images/logo.png`} />
             </div>
-            <h2 className="font-semibold text-[30px] md:text-[50px] xl:text-[80px] tracking-[-5%]">Let’s Work Together</h2>
+            <h2 className="font-semibold text-[30px] md:text-[50px] xl:text-[80px] tracking-[-5%]">
+              Let’s Work Together
+            </h2>
           </span>
           <motion.div style={{ x }} className={styles.buttonContainer}>
             <Rounded className={styles.button}>
-              <p onClick={() => (window.location.href = "http://calendly.com/team-plannorium_")} className="font-medium tracking-[-5%] text-[16px] md:text-[20px] lg:text-[25px]">
+              <p
+                onClick={() =>
+                  (window.location.href =
+                    "http://calendly.com/team-plannorium_")
+                }
+                className="font-medium tracking-[-5%] text-[16px] md:text-[20px] lg:text-[25px]"
+              >
                 Book a Call
               </p>
             </Rounded>
@@ -37,12 +50,22 @@ export default function index() {
 
         <div className={styles.nav}>
           <Rounded backgroundColor={"#0000A3"}>
-            <p onClick={() => (window.location.href = "mailto:hello@plannorium.com")} className="font-medium tracking-[-5%] text-[15px] md:text-[20px] lg:text-[25px]">
-              hello@plannorium.com
+            <p
+              onClick={() =>
+                (window.location.href = "mailto:info@plannorium.com")
+              }
+              className="font-medium tracking-[-5%] text-[15px] md:text-[20px] lg:text-[25px]"
+            >
+              info@plannorium.com
             </p>
           </Rounded>
           <Rounded backgroundColor={"#0000A3"}>
-            <p onClick={() => (window.location.href = "https://wa.me/2348068926547")} className="font-medium tracking-[-5%] text-[15px] md:text-[20px] lg:text-[25px]">
+            <p
+              onClick={() =>
+                (window.location.href = "https://wa.me/2348068926547")
+              }
+              className="font-medium tracking-[-5%] text-[15px] md:text-[20px] lg:text-[25px]"
+            >
               +23 4 80 68 92 65 47
             </p>
           </Rounded>
@@ -52,7 +75,13 @@ export default function index() {
           <div>
             <span>
               <h3>Version</h3>
-              <p onClick={() => (window.location.href = "https://fateme-adiban.vercel.app/")}>2025 © Fateme Adiban</p>
+              <p
+                onClick={() =>
+                  (window.location.href = "https://fateme-adiban.vercel.app/")
+                }
+              >
+                2025 © Fateme Adiban
+              </p>
             </span>
           </div>
 
@@ -61,20 +90,41 @@ export default function index() {
               <h3>Socials</h3>
               <div></div>
               <Magnetic>
-                <p onClick={() => (window.location.href = "https://linkedin.com/company/plannorium")}>LinkedIn</p>
+                <p
+                  onClick={() =>
+                    (window.location.href =
+                      "https://linkedin.com/company/plannorium")
+                  }
+                >
+                  LinkedIn
+                </p>
               </Magnetic>
             </span>
 
             <Magnetic>
-              <p onClick={() => (window.location.href = "https://www.instagram.com/plannorium?igsh=YW10NTU2cWU4c24z")}>Instagram</p>
+              <p
+                onClick={() =>
+                  (window.location.href =
+                    "https://www.instagram.com/plannorium?igsh=YW10NTU2cWU4c24z")
+                }
+              >
+                Instagram
+              </p>
             </Magnetic>
 
             <Magnetic>
-              <p onClick={() => (window.location.href = "https://x.com/plannorium?t=U4112cvWrn8YznM9wZZ8sg&s=09")}>Twitter</p>
+              <p
+                onClick={() =>
+                  (window.location.href =
+                    "https://x.com/plannorium?t=U4112cvWrn8YznM9wZZ8sg&s=09")
+                }
+              >
+                Twitter
+              </p>
             </Magnetic>
           </div>
         </div>
       </div>
     </motion.div>
-  )
+  );
 }
