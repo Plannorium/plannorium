28680d43dfbce980f2849cc4568bb30a5d1a7b01
Mustapha Shuaibu <almussanplanner12@gmail.com>
2025-08-24 17:29:15 +0100

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import PosthogPageView from "../components/PosthogPageView";
-
-
 
 const inter = Inter({
   weight: "400",
@@ -18,6 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
+      <head>
+        <Script src="https://cdn.botpress.cloud/webchat/v3.2/inject.js" />
+        <Script
+          src="https://files.bpcontent.cloud/2025/08/18/12/20250818125118-RTXGWAFW.js"
+          defer
+        />
+      </head>
       <body className={`antialiased`}>
         <PosthogPageView />
         {children}
